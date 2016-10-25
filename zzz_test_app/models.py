dcc9ea94d446328835077ee12b5ebe9636bac363
Paola Gabriela <paola.villa@pucp.pe>
2016-10-25 13:50:00 -0500

    app with players in different groups
    app with players in different groups

--- zzz_test_app/models.py
+++ zzz_test_app/models.py
@@ -13,22 +13,38 @@ Your app description
 
 class Constants(BaseConstants):
     name_in_url = 'zzz_test_app'
-    players_per_group = None
+    players_per_group = 1
+    num_groups = 3
     num_rounds = 1
+    endowment = c(100)
+    efficiency_factor = 1.8
 
 
 class Subsession(BaseSubsession):
     def before_session_starts(self):
-        self.set_group_matrix([[1], [2]])
-
-
+        matrix = self.get_group_matrix()
+        for row in matrix:
+            row.reverse()
+        self.set_group_matrix(matrix)
+        new_structure = [[1], [2], [3],]
+        self.set_group_matrix(new_structure)
+        self.get_group_matrix()
 class Group(BaseGroup):
-    pass
+    total_contribution = models.CurrencyField()
+    individual_share = models.CurrencyField()
+
+    def set_payoffs(self):
+        self.total_contribution = sum(
+            [p.contribution for p in self.get_group_matrix()])
+        self.individual_share = self.total_contribution * Constants.efficiency_factor / Constants.players_per_group
+        for p in self.get_group_matrix():
+            p.payoff = Constants.endowment - p.contribution + self.individual_share
 
 
 class Player(BasePlayer):
-    pass
-
+    contribution = models.CurrencyField(min=0, max=Constants.endowment)
+    def get_partner(self):
+        return self.get_others_in_subsession()[0]
 
 
 
