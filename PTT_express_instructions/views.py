d335ea6c3347f32fa0834809dd4d4123aed624e9
Kristian LopezVargas <kristianlvargas@gmail.com>
2016-11-01 02:41:01 -0700

    instructions :)

--- PTT_express_instructions/views.py
+++ PTT_express_instructions/views.py
@@ -13,19 +13,38 @@ from .models import Constants
 
 
 class Instructions(Page):
+
+    def vars_for_template(self):
+        return {
+            'treatment': self.group.treatment,
+            'role': self.player.role(),
+            'value_type': self.group.value_type,
+            'elicitation_method': self.group.elicitation_method,
+            'BDM_type': self.group.BDM_type,
+            'participation_fee': self.session.config['participation_fee'],
+            'endowment': self.player.endowment,
+            'BDM_uplimit': self.group.BDM_uplimit
+        }
+
+
+class ControlQuestions(Page):
     pass
-
-class ResultsWaitPage(WaitPage):
-
-    def after_all_players_arrive(self):
-        pass
 
 
 class Results(Page):
     pass
 
+# class ResultsWaitPage(WaitPage):
+#
+#     def after_all_players_arrive(self):
+#         pass
+#
+#
+
 
 page_sequence = [
-    Instructions,
-    Results
+    Instructions
+    # ,
+    # ControlQuestions,
+    # Results
 ]
