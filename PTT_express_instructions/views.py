303c34d0720966bf86458ede9cd9c5f30f82b3c3
Kristian LopezVargas <kristianlvargas@gmail.com>
2016-11-08 12:34:37 -0800

    -

--- PTT_express_instructions/views.py
+++ PTT_express_instructions/views.py
@@ -54,7 +54,6 @@ class ControlQuestions(Page):
             return 'The correct answer is  `Not` -- Once you are paired with another participant, ' \
                    'will you NEVER know the identity of the other participant'
 
-
     def ctrlQ_B_earnings_error_message(self, value):
         if self.group.treatment in ['NM', 'FM']:
             if not (value == '13.00 - X'):
