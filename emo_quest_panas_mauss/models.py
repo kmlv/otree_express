00052549a21e89e4773519067e2ac370814d7b91
Paola Gabriela <paola.villa@pucp.pe>
2016-11-23 23:08:31 -0500

    recoding emo_mauss_quest
    recoding emo_mauss_quest according to batson_quest

--- emo_quest_panas_mauss/models.py
+++ emo_quest_panas_mauss/models.py
@@ -18,45 +18,45 @@ class Constants(BaseConstants):
     num_rounds = 1
 
     emo_list = [
-        'Accepted',
-        'Active',
-        'Afraid',
-        'Alert',
-        'Amused',
-        'Angry',
-        'Annoyed',
-        'Anxious',
-        'Ashamed',
-        'Attentive',
-        'Calm',
-        'Contented (Peacefully Happy)',
-        'Determined',
-        'Distressed',
-        'Down',
-        'Embarrassed',
-        'Energetic',
-        'Enthusiastic',
-        'Excited',
-        'Gratitude',
-        'Guilty',
-        'Happy',
-        'Hostile',
-        'Inspired',
-        'Interested',
-        'Irritable',
-        'Jittery',
-        'Lonely',
-        'Loving',
-        'Negative (feel bad)',
-        'Nervous',
-        'Positive (feel good)',
-        'Proud',
-        'Rejected',
-        'Sad',
-        'Scared',
-        'Strong',
-        'Upset'
-        ]
+        ['Accepted', ],
+        ['Active', ],
+        ['Afraid', ],
+        ['Alert', ],
+        ['Amused', ],
+        ['Angry', ],
+        ['Annoyed', ],
+        ['Anxious', ],
+        ['Ashamed', ],
+        ['Attentive', ],
+        ['Calm', ],
+        ['Contented (Peacefully Happy)', ],
+        ['Determined', ],
+        ['Distressed', ],
+        ['Down', ],
+        ['Embarrassed', ],
+        ['Energetic', ],
+        ['Enthusiastic', ],
+        ['Excited', ],
+        ['Gratitude', ],
+        ['Guilty', ],
+        ['Happy', ],
+        ['Hostile', ],
+        ['Inspired', ],
+        ['Interested', ],
+        ['Irritable', ],
+        ['Jittery', ],
+        ['Lonely', ],
+        ['Loving', ],
+        ['Negative (feel bad)', ],
+        ['Nervous', ],
+        ['Positive (feel good)', ],
+        ['Proud', ],
+        ['Rejected', ],
+        ['Sad', ],
+        ['Scared', ],
+        ['Strong', ],
+        ['Upset', ]
+    ]
 
     # shuffle
     emo_list = random.sample(emo_list, len(emo_list))
@@ -64,27 +64,37 @@ class Constants(BaseConstants):
     # list sizes
     emo_list_size = len(emo_list)
     num_emo_pg1 = int(round(emo_list_size / 2, 0))
-    print('len1', num_emo_pg1)
+    emo_list_pg1 = emo_list[:num_emo_pg1]
+    #print('len1', num_emo_pg1)
     num_emo_pg2 = emo_list_size - num_emo_pg1
-    print('len2',num_emo_pg2)
+    emo_list_pg2 = emo_list[num_emo_pg2:]
+    #rint('len2', num_emo_pg2)
 
 
 class Subsession(BaseSubsession):
-
     def before_session_starts(self):
-        print(Constants.emo_list)
+        print(Constants.emo_list_pg1)
+
 
 class Group(BaseGroup):
     pass
 
-class Player(BasePlayer):
 
+class Player(BasePlayer):
     time_emo_quest_pg1 = models.TextField(widget=widgets.HiddenInput(attrs={'id': 'arrive_time'}))
     time_emo_quest_pg2 = models.TextField(widget=widgets.HiddenInput(attrs={'id': 'arrive_time'}))
     time_DoneQuestionnaire = models.TextField(widget=widgets.HiddenInput(attrs={'id': 'arrive_time'}))
 
-    for i in range(len(Constants.emo_list)):
-        temp = Constants.emo_list[i]
+    for i in range(len(Constants.emo_list_pg1)):
+        temp = '_'.join(Constants.emo_list_pg1[i])
+        locals()['{}'.format(temp)] = models.DecimalField(widget=widgets.HiddenInput(),
+                                                          max_digits=3,
+                                                          decimal_places=1,
+                                                          min=1,
+                                                          max=7
+                                                          )
+    for i in range(len(Constants.emo_list_pg2)):
+        temp = '_'.join(Constants.emo_list_pg2[i])
         locals()['{}'.format(temp)] = models.DecimalField(widget=widgets.HiddenInput(),
                                                           max_digits=3,
                                                           decimal_places=1,
--- emo_quest_panas_mauss/views.py
+++ emo_quest_panas_mauss/views.py
@@ -7,34 +7,35 @@ import random
 
 class QuestionnairePage1(Page):
 
-    form_fields = [Constants.emo_list[i] for i in range(0, Constants.num_emo_pg1)]
+    form_fields = ['_'.join(Constants.emo_list_pg1[i]) for i in range(Constants.num_emo_pg1)]
     form_fields.append('time_emo_quest_pg1')
     form_model = models.Player
 
     def vars_for_template(self):
         # we shuffle emotion lists
         n1 = Constants.num_emo_pg1
-        shuffled_emotion_list = random.sample(Constants.emo_list[:n1], n1)
+        shuffled_emotion_list = random.sample(Constants.emo_list_pg1, n1)
         print("page1", shuffled_emotion_list)
         return {
-            'emo_list': shuffled_emotion_list
+            'emo_list_pg1': shuffled_emotion_list
         }
 
 
 class QuestionnairePage2(Page):
-    form_fields = [Constants.emo_list[i] for i in range(Constants.num_emo_pg1, Constants.emo_list_size)]
+    form_fields = ['_'.join(Constants.emo_list_pg2[i]) for i in range(Constants.num_emo_pg2)]
     form_fields.append('time_emo_quest_pg2')
     form_model = models.Player
 
     def vars_for_template(self):
         # we shuffle emotion lists
-        n1 = Constants.num_emo_pg1
+        # n1 = Constants.num_emo_pg1
         n2 = Constants.num_emo_pg2
-        shuffled_emotion_list = random.sample(Constants.emo_list[n1:n1+n2], n2)
+        shuffled_emotion_list = random.sample(Constants.emo_list_pg2, n2)
         print("page2", shuffled_emotion_list)
         return {
-            'emo_list': shuffled_emotion_list
+            'emo_list_pg2': shuffled_emotion_list
         }
+
 
 
 class DoneQuestionnaire(Page):
