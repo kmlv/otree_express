55f39959713e0e82b764e67929ad7536cc068629
Ankur Goswami <agoswam3@ucsc.edu>
2017-01-11 12:15:42 -0800

    Add randomize.js

---
+++ _static/global/randomize.js
@@ -0,0 +1,20 @@
+/*
+* Given: Table with id questions
+* Replace a random row after another random row rows.length number of times
+*/
+var randomize = function (){
+    var rows = $("#randomizeTable > tbody > tr")
+    for(var i = 0; i < rows.length; i++){
+        // Pick a two spots in the table that are not the first row 
+        var spot1 = Math.floor(Math.random() * rows.length + 1)
+        var spot2 = Math.floor(Math.random() * rows.length + 1)
+        var row1 = rows.eq(spot1)
+        var row2 = rows.eq(spot2)
+        // Put row1 after row2
+        $(row1).after($(row2))
+    }
+}
+
+$(document).ready(function(){
+    randomize();
+});
